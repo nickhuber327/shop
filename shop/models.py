60d44fc306cd032e68493d7c46388c72282cd12f
Nick Huber <Nick Huber>
2016-06-15 19:01:45 -0500

    First commit

---
+++ shop/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    date = models.DateTimeField()
+    price = models.FloatField()
+    stock = models.IntegerField()
+    sold = models.IntegerField()
+    rating = models.FloatField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
---
+++ shop/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+from django.views.generic import ListView, DetailView
+from shop.models import Item
+
+urlpatterns = [
+    url(r'^$', ListView.as_view(queryset=Item.objects.all().order_by("-date") [:25], template_name="shop/shop.html")),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Item, template_name = 'shop/item.html'))
+]
